7e18f94c636b88e26405cd52e857022c24e86b24
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2022-07-03 00:34:57 -0400

    add all user validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,13 +3,20 @@ const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
 const keys = require("../../config/keys")
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login')
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the user route" });
 });
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
@@ -36,6 +43,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email
     const password = req.body.password
 
